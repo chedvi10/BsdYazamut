7f6698a4f2cc691ae2ca27c0d0e3ccd41210f74c
chedvi10 <c0556731959@gmail.com>
2025-01-22 23:52:58 +0200

    add contact form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent{
+  contactForm?: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      city: ['', Validators.required],
+      address: ['', Validators.required],
+      reason: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm?.valid) {
+      console.log(this.contactForm.value);
+      // כאן תוכל להוסיף לוגיקה לשליחת הנתונים
+    }
+  }
 
 }
